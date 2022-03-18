2681e00b162afcee0248d05737b2ad5fcebb5354
Tom Redon <redon.tom@gmail.com>
2022-03-18 08:22:15 +0100

    [ADD #3] - Added the draw app part

--- app/src/main/java/com/example/stackapp/DrawActivity.java
+++ app/src/main/java/com/example/stackapp/DrawActivity.java
@@ -12,6 +12,6 @@ public class DrawActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_draw);
+        setContentView(new DrawView(this));
     }
 }
