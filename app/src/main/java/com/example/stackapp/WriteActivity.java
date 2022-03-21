f604267d9edea3f053134422d0172ea0eb7bbac6
Tom Redon <redon.tom@gmail.com>
2022-03-21 17:37:33 +0100

    [ADD #4] - Writing app working

--- app/src/main/java/com/example/stackapp/WriteActivity.java
+++ app/src/main/java/com/example/stackapp/WriteActivity.java
@@ -1,6 +1,10 @@
 package com.example.stackapp;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -10,5 +14,16 @@ public class WriteActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_write);
+
+        Button but = (Button) findViewById(R.id.button);
+        EditText edit = (EditText) findViewById(R.id.textInputEditText2);
+        TextView text = (TextView) findViewById(R.id.textView2);
+
+        but.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                text.setText(edit.getText().toString());
+            }
+        });
     }
 }
