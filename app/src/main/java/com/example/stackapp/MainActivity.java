efd0705c6e0239ca49d04bad501632ad4321b0e9
Tom Redon <redon.tom@gmail.com>
2022-03-27 16:37:37 +0200

    [UPDATE #4] - Corrected Geoloc app

--- app/src/main/java/com/example/stackapp/MainActivity.java
+++ app/src/main/java/com/example/stackapp/MainActivity.java
@@ -1,13 +1,35 @@
 package com.example.stackapp;
 
+import static android.Manifest.permission.ACCESS_COARSE_LOCATION;
+import static android.Manifest.permission.ACCESS_FINE_LOCATION;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.location.Location;
+import android.location.LocationListener;
+import android.location.LocationManager;
+import android.os.Build;
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
 
+    private float longitude;
+    private float latitude;
+
+    private static final String[] LOCATION_PERMS={
+            ACCESS_FINE_LOCATION,
+            ACCESS_COARSE_LOCATION
+    };
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -38,9 +60,29 @@ public class MainActivity extends AppCompatActivity {
             startActivity(new Intent(this, AudioActivity.class));
         });
 
+        if (checkSelfPermission(ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED
+                && checkSelfPermission(ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            requestPermissions(LOCATION_PERMS, 1337);
+        }
+
+        LocationManager lm = (LocationManager) getSystemService(Context.LOCATION_SERVICE);
+        lm.requestLocationUpdates(LocationManager.GPS_PROVIDER, 0, 0, new LocationListener() {
+            @Override
+            public void onLocationChanged(@NonNull Location location) {
+                return ;
+            }
+        });
+        if (lm != null) {
+            Location loc = lm.getLastKnownLocation(LocationManager.GPS_PROVIDER);
+            if (loc != null) {
+                latitude = (float) loc.getLatitude();
+                longitude = (float) loc.getLongitude();
+            }
+        }
+
         Button geo = findViewById(R.id.geoButton);
         geo.setOnClickListener( (v) -> {
-            startActivity(new Intent(this, GeolocActivity.class));
+            Log.i("Link", "https://www.google.com/maps/search/?api=1&query=" + latitude + "," + longitude);
         });
     }
 }
